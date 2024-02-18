2dbfe33a115edef2782a78c87526083efad959a6
bucsan <thiago.bucsan@gmail.com>
2024-02-17 22:52:05 -0300

    Organizando a Arquitetura dos Primeiros Microsserviços

---
+++ GeekShooping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new 
+                ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindtAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindtById(id);
+            if (product.Id <= 0) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        {
+            if (vo is null) return BadRequest();
+            var product = await _productRepository.Create(vo);
+            return Ok(vo);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        {
+            if (vo is null) return BadRequest();
+            var product = await _productRepository.Update(vo);
+            return Ok(vo);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(long id) 
+        {
+            var status = await _productRepository.Delete(id);
+            if(!status) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
---
+++ GeekShooping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShopping.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductVO>> FindtAll();
+        Task<ProductVO> FindtById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
+    }
+}
